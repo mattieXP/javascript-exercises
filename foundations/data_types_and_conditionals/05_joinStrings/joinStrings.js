5c90503ab6b753ea9844a43c44c0e16c715077e7
mattieXP <mattieexperiences@gmail.com>
2026-09-06 10:09:56 +0200

    Complete JavaScript data types & conditionals Exercises
    Basic JavaScript tests form The Odin Project course

--- foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
+++ foundations/data_types_and_conditionals/05_joinStrings/joinStrings.js
@@ -1,15 +1,17 @@
-/*
- First name: Carlos
- Last name: Stevenson
- This year: 1965
- Birth year: 1947
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
+ const firstName = "Carlos";
+ const lastName = "Stevenson";
+ const thisYear = 1965;
+ const birthYear = 1947;
+ let fullName = firstName + ' ' + lastName;
+ let age = thisYear - birthYear;
+ let greeting = 'Hello! My name is ' + fullName + ' and I am ' + age + ' years old.'
+ /*The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
 */
 
 // Add your code right below, good luck!
 
-
+console.log(greeting);
 
 
 
