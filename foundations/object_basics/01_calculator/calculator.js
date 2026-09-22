7c1b9d5c0a169a9b86ebe8a3dcc38c5d8d3fe689
mattieXP <mattieexperiences@gmail.com>
2026-09-22 12:37:07 +0200

    Complete JS Calculator exercise

--- foundations/object_basics/01_calculator/calculator.js
+++ foundations/object_basics/01_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const totalSum = array.reduce(function(currentNumber, nextNumber ) {
+    return currentNumber += nextNumber;
+  }, 0);
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const total = array.reduce((currentNumber, nextNumber) => {
+    return currentNumber *= nextNumber;
+  }, 1);
+  return total;
 };
 
-const power = function() {
-	
-};
+const power = function(a,b) {
+	return a ** b;
+}
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let array = [];
+  for (let i = 1; i <= a; i++) {
+    array.unshift(i);
+ }
+ const fact = array.reduce((total, number) => {
+    return total * number;
+ }, 1);
+ return fact;
 };
 
 // Do not edit below this line
