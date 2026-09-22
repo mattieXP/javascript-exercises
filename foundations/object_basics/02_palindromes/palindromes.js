6f141b5ad11115cb91570b043c6032bcf9a25057
mattieXP <mattieexperiences@gmail.com>
2026-09-22 14:33:52 +0200

    Complete Palindromes JS Objects Exercise

--- foundations/object_basics/02_palindromes/palindromes.js
+++ foundations/object_basics/02_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const lowerCaseString = string.toLowerCase();
+    const turnInArray = Array.from(lowerCaseString);
+    const cleanArray = turnInArray.filter((char) => {
+        if (char != " " && char != "," && char != ";" && char != "." && char!="!" && char!="?") {
+            return true;
+        };
+    });
+    const array = cleanArray.slice(0 , cleanArray.length);
+    const reversed = cleanArray.reverse();
 
+    const orderedString = array.toString();
+    const reversedString = reversed.toString();
+
+    return orderedString === reversedString; 
 };
 
 // Do not edit below this line
