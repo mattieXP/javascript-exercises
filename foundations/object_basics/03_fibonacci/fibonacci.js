e92a38ece0aef81fb69ece9703571ee607345bb0
mattieXP <mattieexperiences@gmail.com>
2026-09-23 18:30:10 +0200

    Complete JavaScript Fibonacci Exercise

--- foundations/object_basics/03_fibonacci/fibonacci.js
+++ foundations/object_basics/03_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    
+    const array = [0,1,1];
 
+    if (index <0) {
+        return "OOPS"
+    } else {
+        for (let i = 3 ; i<= index ; i++) {
+            const antepenultimate = i - 2;
+            const penultimate = i - 1;
+            const newNumber = array[antepenultimate] + array[penultimate];
+            array.push(newNumber);
+        }
+        return array[index];
+        }
 };
 
 // Do not edit below this line
