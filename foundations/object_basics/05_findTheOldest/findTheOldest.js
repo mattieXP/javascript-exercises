8585234aadf5fd194c99e6d0b7e98b822e03d171
mattieXP <mattieexperiences@gmail.com>
2026-09-24 11:07:00 +0200

    Complete findTheOldest JS Exercise

--- foundations/object_basics/05_findTheOldest/findTheOldest.js
+++ foundations/object_basics/05_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(listOfPeople) {
+    const lifeLength = function(people) {
+        let age;
+        if (typeof people.yearOfDeath == 'undefined') {
+           age = new Date().getFullYear() - people.yearOfBirth;
+        } else {
+            age = people.yearOfDeath - people.yearOfBirth;}
+        return age;
+    }
 
+    return listOfPeople.reduce((previousPeople, nextPeople) => {
+        if (lifeLength(previousPeople) < lifeLength(nextPeople)) {
+            return nextPeople;
+        } else return previousPeople;
+    });
 };
 
 // Do not edit below this line
